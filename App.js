e04bd96cc696de84b34fa9a32b3c15135b124b28
KimMyeonggeon <nds3274@naver.com>
2023-06-22 15:17:12 +0900

    명건_수정

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 
+import BackgroundImage from './assets/background.png';
 
 export default function App() {
   return (
@@ -16,6 +17,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // 배경 이미지를 커버할 수 있도록 설정
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
