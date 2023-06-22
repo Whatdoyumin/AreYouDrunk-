73f57c93a33672e3018161517eed4f88d47b1921
Whatdoyumin <lym4963@naver.com>
2023-06-22 16:26:48 +0900

    메인화면구현

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import React from 'react';
 
+import BackgroundImage from './assets/main_screen.png'; // 배경 이미지 파일 경로에 맞게 수정해주세요
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>도대체 뭐가 문제였을까? 음..</Text>
+      <ImageBackground
+        source={BackgroundImage}
+        style={styles.backgroundImage}
+      ></ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +20,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', // 배경 이미지를 커버할 수 있도록 설정
+    width: '100%',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
