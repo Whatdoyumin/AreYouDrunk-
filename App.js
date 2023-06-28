38a0843ac3979abb7e0f336362e3d09d2fe90c5b
Whatdoyumin <lym4963@naver.com>
2023-06-28 15:27:52 +0900

    push받기

--- App.js
+++ App.js
@@ -1,18 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Button } from 'react-native';
 import React from 'react';
+
+import "react-native-gesture-handler";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+
+const Stack = createStackNavigator();
 
 import BackgroundImage from './assets/main_screen.png'; // 배경 이미지 파일 경로에 맞게 수정해주세요
 
-export default function App() {
+function Home({navigation}) {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={BackgroundImage}
-        style={styles.backgroundImage}
-      ></ImageBackground>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity style={{flex : 1}} onPress={() =>
+      navigation.navigate('introPage')}>
+        <View style={styles.container}>
+          <ImageBackground
+            source={BackgroundImage}
+            style={styles.backgroundImage}
+          ></ImageBackground>
+          <StatusBar style="auto" />
+
+          <Button
+            onPress={() => navigation.navigate('introPage')}
+            title="게임 설명">
+          </Button>
+
+        </View>
+      </TouchableOpacity>
   );
 }
 
@@ -32,3 +48,34 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+function IntroPage(){
+  return (
+    <View style={{ flex: 1, alignItems: 'right', 
+    justifyContent: 'center'}}>
+      <Text>게임 설명 페이지다 ㅋ</Text>
+
+    </View>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown:false}}
+        />
+        <Stack.Screen
+          name="introPage"
+          component={IntroPage}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
