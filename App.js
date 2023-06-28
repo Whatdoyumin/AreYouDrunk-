673cd618501a5643fab57e8a629aefb2f2999acf
KimMyeonggeon <nds3274@naver.com>
2023-06-28 16:37:35 +0900

    js파일_구분

--- App.js
+++ App.js
@@ -1,34 +1,57 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  Button,
+} from 'react-native';
 import React from 'react';
 
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
 
 import BackgroundImage from './assets/main_screen.png'; // 배경 이미지 파일 경로에 맞게 수정해주세요
+// import IntroPage from
+// inport RecodePage from
 
-function Home({navigation}) {
+function Home({ navigation }) {
   return (
-    <TouchableOpacity style={{flex : 1}} onPress={() =>
-      navigation.navigate('introPage')}>
-        <View style={styles.container}>
-          <ImageBackground
-            source={BackgroundImage}
-            style={styles.backgroundImage}
-          ></ImageBackground>
-          <StatusBar style="auto" />
+    <View style={styles.container}>
+      <ImageBackground source={BackgroundImage} style={styles.backgroundImage}>
+        <StatusBar style="auto" />
 
-          <Button
-            onPress={() => navigation.navigate('introPage')}
-            title="게임 설명">
-          </Button>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => navigation.navigate('IntroPage')}
+        >
+          <Text style={styles.buttonText}>게임 설명</Text>
+        </TouchableOpacity>
 
-        </View>
-      </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button2}
+          onPress={() => navigation.navigate('RecodePage')}
+        >
+          <Text style={styles.buttonText}>전적보기</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button3}
+          onPress={() => navigation.navigate('GameStart')}
+        ></TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button4}
+          onPress={() => navigation.navigate('Makedrink')}
+        >
+          <Text style={styles.buttonText}>폭탄주 제조</Text>
+        </TouchableOpacity>
+      </ImageBackground>
+    </View>
   );
 }
 
@@ -36,26 +59,94 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover', // 배경 이미지를 커버할 수 있도록 설정
+    resizeMode: 'cover',
     width: '100%',
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button: {
+    position: 'absolute',
+    top: 375,
+    right: -8,
+    width: 180,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+  
+  button2: {
+    position: 'absolute',
+    top: 470,
+    right: -8,
+    width: 180,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+
+  button3: {
+    position: 'absolute',
+    top: 450,
+    left: 50,
+    width: 150,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+
+  button4: {
+    position: 'absolute',
+    top: 570,
+    right: -8,
+    width: 180,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+
+  buttonText: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 27,
+  },
 });
 
-
-function IntroPage(){
+function IntroPage() {
   return (
-    <View style={{ flex: 1, alignItems: 'right', 
-    justifyContent: 'center'}}>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>게임 설명 페이지다 ㅋ</Text>
+    </View>
+  );
+}
 
+function RecodePage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> 전적 보기 </Text>
+    </View>
+  );
+}
+
+function GameStart() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> game start </Text>
+    </View>
+  );
+}
+
+function Makedrink() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> make drink </Text>
     </View>
   );
 }
@@ -67,12 +158,12 @@ function App() {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{headerShown:false}}
+          options={{ headerShown: false }}
         />
-        <Stack.Screen
-          name="introPage"
-          component={IntroPage}
-        />
+        <Stack.Screen name="IntroPage" component={IntroPage} />
+        <Stack.Screen name="RecodePage" component={RecodePage} />
+        <Stack.Screen name="GameStart" component={GameStart} />
+        <Stack.Screen name="Makedrink" component={Makedrink} />
       </Stack.Navigator>
     </NavigationContainer>
   );
