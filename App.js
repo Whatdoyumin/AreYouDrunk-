92134a967564dd5010b183b92ccd3e698b57aaab
KimMyeonggeon <nds3274@naver.com>
2023-06-28 16:22:26 +0900

    버튼수정

--- App.js
+++ App.js
@@ -16,6 +16,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 const Stack = createStackNavigator();
 
 import BackgroundImage from './assets/main_screen.png'; // 배경 이미지 파일 경로에 맞게 수정해주세요
+// import IntroPage from
+// inport RecodePage from
 
 function Home({ navigation }) {
   return (
@@ -25,9 +27,28 @@ function Home({ navigation }) {
 
         <TouchableOpacity
           style={styles.button}
-          onPress={() => navigation.navigate('introPage')}
+          onPress={() => navigation.navigate('IntroPage')}
         >
           <Text style={styles.buttonText}>게임 설명</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button2}
+          onPress={() => navigation.navigate('RecodePage')}
+        >
+          <Text style={styles.buttonText}>전적보기</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button3}
+          onPress={() => navigation.navigate('GameStart')}
+        ></TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.button4}
+          onPress={() => navigation.navigate('Makedrink')}
+        >
+          <Text style={styles.buttonText}>폭탄주 제조</Text>
         </TouchableOpacity>
       </ImageBackground>
     </View>
@@ -57,6 +78,40 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 5,
   },
+  
+  button2: {
+    position: 'absolute',
+    top: 470,
+    right: -8,
+    width: 180,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+
+  button3: {
+    position: 'absolute',
+    top: 450,
+    left: 50,
+    width: 150,
+    height: 150,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+
+  button4: {
+    position: 'absolute',
+    top: 570,
+    right: -8,
+    width: 180,
+    height: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 5,
+  },
+
   buttonText: {
     color: 'black',
     fontWeight: 'bold',
@@ -72,6 +127,30 @@ function IntroPage() {
   );
 }
 
+function RecodePage() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> 전적 보기 </Text>
+    </View>
+  );
+}
+
+function GameStart() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> game start </Text>
+    </View>
+  );
+}
+
+function Makedrink() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text> make drink </Text>
+    </View>
+  );
+}
+
 function App() {
   return (
     <NavigationContainer>
@@ -81,7 +160,10 @@ function App() {
           component={Home}
           options={{ headerShown: false }}
         />
-        <Stack.Screen name="introPage" component={IntroPage} />
+        <Stack.Screen name="IntroPage" component={IntroPage} />
+        <Stack.Screen name="RecodePage" component={RecodePage} />
+        <Stack.Screen name="GameStart" component={GameStart} />
+        <Stack.Screen name="Makedrink" component={Makedrink} />
       </Stack.Navigator>
     </NavigationContainer>
   );
